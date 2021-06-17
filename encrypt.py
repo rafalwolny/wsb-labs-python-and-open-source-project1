918e956752d2edbc5ab926ec61ef3077505a7e01
rafalwolny <rafal.wolny96@gmail.com>
2021-06-17 08:02:44 +0200

    initial commit, main.py added with most functionality, now working on modules in encrypt.py

---
+++ encrypt.py
@@ -0,0 +1,12 @@
+someText = ''
+
+def szyfrowanie():
+  someText = input('Podaj tekst: ')
+  print('to jest funkcja SZYFROWANIA z pliku encrypt! tekst do szyfrowania:', someText)
+
+def deszyfrowanie():
+  someText = input('Podaj tekst: ')
+  print('to jest funkcja DESZYFROWANIA z pliku encrypt! tekst do szyfrowania:', someText)
+
+#def printuj():
+#  print('printuje text w pliku encrypted:', someText)
