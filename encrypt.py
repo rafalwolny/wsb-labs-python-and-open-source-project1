dfdb1331d25dd1063d748235c1cac9a67cc51938
rafalwolny <rafal.wolny96@gmail.com>
2021-06-27 15:43:01 +0200

    First stable version of the script is done! Bugs fixed, functionality added and working as expected

--- encrypt.py
+++ encrypt.py
@@ -1,12 +1,34 @@
 someText = ''
 
+szyfrDict = {
+    "a": "y",
+    "e": "i",
+    "i": "o",
+    "o": "a",
+    "y": "e"
+}
+
+
 def szyfrowanie():
-  someText = input('Podaj tekst: ')
-  print('to jest funkcja SZYFROWANIA z pliku encrypt! tekst do szyfrowania:', someText)
+    someText = input('Podaj tekst: ')
+    zaszyfrowane = list(someText)
+    for i in range(len(zaszyfrowane)):
+        for x, y in szyfrDict.items():
+            if zaszyfrowane[i] == x:
+                zaszyfrowane[i] = y
+                break
+    print('Wynik:', ''.join(zaszyfrowane))
+
 
 def deszyfrowanie():
-  someText = input('Podaj tekst: ')
-  print('to jest funkcja DESZYFROWANIA z pliku encrypt! tekst do szyfrowania:', someText)
-
-#def printuj():
-#  print('printuje text w pliku encrypted:', someText)
+    someText = input('Podaj tekst: ')
+    rozszyfrowane = list(someText)
+    for i in range(len(rozszyfrowane)):
+        for x, y in szyfrDict.items():
+            if rozszyfrowane[i] == y:
+                # print('Znaleziono litere z klucza:', rozszyfrowane[i])
+                # print('Ta sama litera ale printowana ze slownika:', y)
+                # print('Trzeba zamienic na:', x)
+                rozszyfrowane[i] = x
+                break
+    print('Wynik:', ''.join(rozszyfrowane))
--- main.py
+++ main.py
@@ -3,13 +3,15 @@ from encrypt import szyfrowanie, deszyfrowanie
 operation = ''
 
 while operation != 'wyjscie':
-  operation = input('Wybierz operacje (szyfrowanie, deszyfrowanie, wyjscie): ')
+    operation = input(
+        'Wybierz operacje (szyfrowanie, deszyfrowanie, wyjscie): ')
 
-  if operation == 'szyfrowanie':
-    szyfrowanie()                                  # <--- tutaj program przechodzi do skryptu szyfrujacego
-  elif operation == 'deszyfrowanie':
-    deszyfrowanie()                                # <--- tutaj program przechodzi do skryptu deszyfrujacego
-  elif operation != ('szyfrowanie' and 'deszyfrowanie' and 'wyjscie'):
-    print('Bledna operacja! Niepoprawne dane')
-    print('Sprobuj ponownie')
-    
+    if operation == 'szyfrowanie':
+        # <--- tutaj program przechodzi do skryptu szyfrujacego
+        szyfrowanie()
+    elif operation == 'deszyfrowanie':
+        # <--- tutaj program przechodzi do skryptu deszyfrujacego
+        deszyfrowanie()
+    elif operation != ('szyfrowanie' and 'deszyfrowanie' and 'wyjscie'):
+        print('Bledna operacja!')
+        print('Sprobuj ponownie')
